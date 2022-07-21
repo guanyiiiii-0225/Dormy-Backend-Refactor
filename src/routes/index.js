e610918cb33a8ff6e165041105149502cba8e17f
guanyiiiii-0225 <zoememy@gmail.com>
2022-07-21 14:07:35 +0800

    add test api

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,12 @@ const express = require('express');
 const auth = require("../middleware/auth");
 const router = express.Router()
 
-router.get("/test", auth, (req, res) => {
+router.get("/test/auth", auth, (req, res) => {
     res.send({message: "token success"});
+})
+
+router.get("/test", (req, res) => {
+    res.send({message: "success"});
 })
 
 module.exports = router;
