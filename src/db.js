c34aa86518318a43ae29bbf98da2a3824c41cf21
guanyiiiii-0225 <zoememy@gmail.com>
2022-07-22 13:54:51 +0800

    fix .env

--- src/db.js
+++ src/db.js
@@ -1,12 +1,13 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
+const dotenv = require("dotenv-defaults") ;
+dotenv.config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Dormy_db',
-  password: 'password',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
-// module.exports.pool = pool.getConnection();
 exports.pool = pool;
