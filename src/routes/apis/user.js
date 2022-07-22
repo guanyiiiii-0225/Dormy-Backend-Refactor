a18374b625c275106855df2cf5bf5c27326953eb
gary1030 <gary6658@ntu.im>
2022-07-22 11:35:45 +0800

    fix db pool

--- src/routes/apis/user.js
+++ src/routes/apis/user.js
@@ -1,12 +1,18 @@
-const pool = require('../../db')
+const db = require('../../db')
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
+  userQuery =  'SELECT * FROM users ORDER BY id ASC';
+  db.pool(function(err, con) { // the function is called when you have a connection
+    if(err) throw err; // or handle it differently than throwing it
+    console.log("con: " + con); // not undefined anymore
+    con.query(userQuery,function(error, res){
+        if (error) {
+          throw error
+        }
+        response.status(200).json(res.rows)
+        con.release();
     })
+  })
 }
 
 module.exports = {
