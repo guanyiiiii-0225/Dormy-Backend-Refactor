71d5bba86cef803f02848ce5cf6bd01e27831ac2
gary1030 <gary6658@ntu.im>
2022-07-22 10:33:05 +0800

    create swagger

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv-defaults');
 const allRoute = require('./src/routes/index');
-const db = require('./src/mongo');
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+// const db = require('./src/mongo');
 const cors = require('cors');
 dotenv.config();
 
-db.on("error", (err) => console.log(err));
-    db.once("open", async () => {
-      const app = express();
+// db.on("error", (err) => console.log(err));
 
-      app.use(express.json());
-      app.use(express.urlencoded({ extended: true }));
+const app = express();
 
-      // init middleware
-      app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-      // define routes
-      app.use('/api', allRoute)
+// init middleware
+app.use(cors())
 
-      const port = process.env.PORT || 4000
-      app.listen(port, () =>
-        console.log(`listening on port ${port}!`),
-      );
-    });
+// define routes
+app.use('/api', allRoute)
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+const port = process.env.PORT || 4000
+app.listen(port, () =>
+  console.log(`listening on port ${port}!`),
+);
+// db.once("open", async () => {
+// });
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,14 @@ const auth = require("../middleware/auth");
 const router = express.Router()
 
 router.get("/test/auth", auth, (req, res) => {
+    /* 	#swagger.tags = ['Test']
+        #swagger.description = 'For testing.' */
     res.send({message: "token success"});
 })
 
 router.get("/test", (req, res) => {
+    /* 	#swagger.tags = ['Test']
+        #swagger.description = 'For testing.' */
     res.send({message: "success"});
 })
 
