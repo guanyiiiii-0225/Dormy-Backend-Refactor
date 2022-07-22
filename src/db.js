63c0d02e0ec3ca90f61d23fb8cc84fba685315aa
guanyiiiii-0225 <zoememy@gmail.com>
2022-07-22 13:36:29 +0800

    fix db pool

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
-const Pool = require('pg').Pool
+const { Pool } = require('pg')
+
 const pool = new Pool({
-  user: 'user',
+  user: 'postgres',
   host: 'localhost',
   database: 'Dormy_db',
   password: 'password',
   port: 5432,
 })
 
-module.exports.pool = pool.getConnection;
+// module.exports.pool = pool.getConnection();
+exports.pool = pool;
--- src/routes/apis/user.js
+++ src/routes/apis/user.js
@@ -1,18 +1,9 @@
-const db = require('../../db')
+const pool = require('../../db').pool
 
-const getUsers = (request, response) => {
-  userQuery =  'SELECT * FROM users ORDER BY id ASC';
-  db.pool(function(err, con) { // the function is called when you have a connection
-    if(err) throw err; // or handle it differently than throwing it
-    console.log("con: " + con); // not undefined anymore
-    con.query(userQuery,function(error, res){
-        if (error) {
-          throw error
-        }
-        response.status(200).json(res.rows)
-        con.release();
-    })
-  })
+const getUsers = async (request, response) => {
+    userQuery =  'SELECT * FROM users ORDER BY id ASC';
+    res = await pool.query(userQuery)
+    response.status(200).json(res.rows)
 }
 
 module.exports = {
